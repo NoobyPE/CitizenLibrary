c942a3a7179ab77949ee600ddf2378838ec4fa49
srclauyt@gmail.com <srclauyt@gmail.com>
2023-09-20 12:59:45 -0300

    minor update

--- src/nooby/CitizenLibrary/CitizenLibrary.php
+++ src/nooby/CitizenLibrary/CitizenLibrary.php
@@ -3,7 +3,7 @@
 namespace nooby\CitizenLibrary;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\SingletonTrait;
+//use pocketmine\utils\SingletonTrait;
 
 use nooby\CitizenLibrary\controller\Controller;
 use nooby\CitizenLibrary\controller\DefaultController;
@@ -11,39 +11,36 @@ use nooby\CitizenLibrary\factory\CitizenFactory;
 
 class CitizenLibrary {
 	
-        use SingletonTrait;
-	
 	private PluginBase $plugin;
+	
 	private CitizenFactory $citizenFactory;
 	
 	public static function create(PluginBase $plugin): self
 	{
-		return new CitizenLibrary($plugin, new DefaultController());
+		return new self($plugin, new DefaultController());
 	}
 	
 	public function __construct(PluginBase $plugin, Controller $customController)
 	{
-		self::setInstance(this);
 		$this->plugin = $plugin;
 		$this->citizenFactory = new CitizenFactory();
 		$plugin->getServer()->getPluginManager()->registerEvents($customController, $plugin);
-	
-
-     }
+	}
 
     /**
      * @return PluginBase
      */
-    public function getPlugin(): PluginBase
-    {
-        return $this->plugin;
-    }
+  public function getPlugin(): PluginBase
+  {
+    return $this->plugin;
+  }
 
     /**
      * @return CitizenFactory
      */
-    public function getCitizenFactory(): CitizenFactory
-    {
-        return $this->citizenFactory;
-    }
+  public function getFactory(): CitizenFactory
+  {
+    return $this->citizenFactory;
+  }
+    
 }
